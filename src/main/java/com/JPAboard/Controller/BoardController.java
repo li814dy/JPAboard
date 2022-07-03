bbd9fa32c1be22bd769a53c1bb34fcca1bf23e80
li814dy <li814dy@gmail.com>
2022-07-04 04:02:51 +0900

    fixing error...

---
+++ src/main/java/com/JPAboard/Controller/BoardController.java
@@ -0,0 +1,31 @@
+package com.JPAboard.Controller;
+
+import com.JPAboard.dto.BoardDto;
+import com.JPAboard.service.BoardService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@AllArgsConstructor
+public class BoardController {
+    private BoardService boardService;
+
+    @GetMapping("/")
+    public String list() {
+        return "board/list.html";
+    }
+
+    @GetMapping("/post")
+    public String write() {
+        return "board/write.html";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/JPAboard/service/BoardService.java
@@ -0,0 +1,19 @@
+package com.JPAboard.service;
+
+import com.JPAboard.domain.repository.BoardRepository;
+import com.JPAboard.dto.BoardDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@AllArgsConstructor
+@Service
+public class BoardService {
+    private BoardRepository boardRepository;
+
+    @Transactional
+    public Long savePost(BoardDto boardDto) {
+        return boardRepository.save(boardDto.toEntity()).getId();
+    }
+}
